717d14044c3b6675a0e0009e36b51534e8eeef68
NielsKolkman <60409592+Niels-Kolkman@users.noreply.github.com>
2020-10-08 10:08:01 +0200

    look

--- CustomerProject/templates/Tests/edit.php
+++ CustomerProject/templates/Tests/edit.php
@@ -28,16 +28,16 @@
                     echo $this->Form->control('start_time');
                     echo $this->Form->control('end_time');
 
-                echo '<div class="container checkbox-list-box">';
                 echo '<label for="group_id">Groups</label>';
+                echo '<div class="container checkbox-list-box">';
                 echo $this->Form->select('group_id', $groups, [
                     'multiple' => 'checkbox',
                     'default' => array_column($selectedGroups, 'groups_id')
                 ]);
                 echo '</div>';
 
-                echo '<div class="container checkbox-list-box">';
                 echo '<label for="users_id">Students</label>';
+                echo '<div class="container checkbox-list-box">';
                 echo $this->Form->select('user_id', $users ,[
                     'multiple' => 'checkbox',
                     'default' => array_column($selectedUsers, 'users_id')
